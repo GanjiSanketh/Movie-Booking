c342f54d72fab743b32f5fc6f6edfb29b50dc188
sankethg <gsanketh7121@gmail.com>
2025-01-15 01:08:52 +0530

    Added components for Login and Registeration

--- movie-UI/src/app/app.component.ts
+++ movie-UI/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true
 })
 export class AppComponent {
   title = 'movie-UI';
